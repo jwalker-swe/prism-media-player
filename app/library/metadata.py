9c45426dbd794bdfc0d0e3c43f517cd38610d91a
jwalker-swe <jwalker.swe@gmail.com>
2026-03-10 02:33:54 -0500

    feat: ability to extract metadata from audio files based on path, capable of handling lists of audio files

--- app/library/metadata.py
+++ app/library/metadata.py
@@ -1,10 +1,36 @@
 # Current Goal: Return song info based on list of songs given
 
 from pathlib import Path 
-import mutagen
+from mutagen import File
+from mutagen.easyid3 import EasyID3
+from scanner import scan
+
+
 
 """
  Sudo code steps:
     1. Get list of songs
     2. For each song get metadata and store in new list
 """
+
+metadata_list = []
+
+def extract_metadata(path):
+    try:
+        audio = File(path, easy=True)
+
+        return {
+            "path": path,
+            "album": audio.get("album", [None])[0],
+            "title": audio.get("title", [None])[0],
+            "artist": audio.get("artist", [None])[0],
+            "duration": audio.info.length
+        }
+    except Exception as e:
+        print(f"Failed to read metadata for {path}: {e}")
+        return None
+
+
+data = extract_metadata("/Users/jordan/Music/tyler_the_creator/dont_tap_the_glass/01.flac")
+
+print(data)
